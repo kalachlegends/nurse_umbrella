8bd3fb7bbbd7f7861e5e49d4e39bfb8de978c559
artem <artem.kalachev.2002@gmail.com>
2022-12-04 11:20:13 +0600

    govno

--- apps/nurse_frotend/src/views/docs/DocsCreateView.vue
+++ apps/nurse_frotend/src/views/docs/DocsCreateView.vue
@@ -1,11 +1,43 @@
 <template lang="">
-    <div>
-        <h1 style="text-align: center;">Выберите шаблон</h1>
-        <a-template title="Артем" name="артем"></a-template>
+    <h1 style="text-align: center;">Выберите шаблон</h1>
+    <div class="grid">
+        <a-template v-if="doc.length != 0" v-for="item in doc" 
+        :title="item.title"
+        
+        :anemesis="item.anemesis"   
+        :report_customer="item.report_customer"
+        :exam="item.exam"
+        :id="item.id"
+        ></a-template>
+        <h3 v-else > Нету шаблонов</h3>
+
+        
     </div>
 </template>
 <script>
-export default {};
+import axios from "@/axios";
+import { ref, onMounted, computed, inject } from "vue";
+export default {
+  setup() {
+    const doc = ref([]);
+    const isLoad = inject("isLoad");
+    onMounted(async () => {
+      isLoad.value = true;
+      const data = await axios.get("/template");
+      doc.value = data.data.data;
+      console.log(doc.value);
+      isLoad.value = false;
+    });
+    return {
+      doc,
+    };
+  },
+};
 </script>
 <style lang="scss" scoped>
+.grid {
+  display: grid;
+  grid-template: 1fr/1fr 1fr 1fr;
+  gap: 10px;
+}
 </style>
--- apps/nurse_frotend/src/views/docs/DocsViewId.vue
+++ apps/nurse_frotend/src/views/docs/DocsViewId.vue
@@ -39,7 +39,7 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss" >
 @import "@/assets/scss/colors.scss";
 
 .doc {
